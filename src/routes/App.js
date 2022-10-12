2d01928c6f5e0597c61a95affd5127d0d0b1bca6
Federico Barrientos <fgbarrientos93@gmail.com>
2022-10-12 20:28:38 +0200

    Init

---
+++ src/routes/App.js
@@ -0,0 +1,17 @@
+
+
+function App() {
+  return (
+    <div className="App">
+      <link
+        rel="stylesheet"
+        href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css"
+        integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor"
+        crossOrigin="anonymous"
+      />
+      <h1>Soy Dev Store</h1>
+    </div>
+  );
+}
+
+export default App;
